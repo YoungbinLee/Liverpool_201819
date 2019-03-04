e87147051121802329f1b3afabc314f07b3b07c8
Youngbin Lee <kndlyb@gmail.com>
2019-03-04 14:55:50 +0900

    update round 29

--- PL/Round_29_EVELIV.R
+++ PL/Round_29_EVELIV.R
@@ -7,9 +7,9 @@ CairoFonts(  # slight mod to example in ?CairoFonts page
     bolditalic="FreeSans:style=BoldOblique"
 )
 
-Hometeam = 'Liverpool'
-Awayteam = 'Watford'
-No_round = 28
+Hometeam = 'Everton'
+Awayteam = 'Liverpool'
+No_round = 29
 
 CairoPDF(file = paste("PL/PREMIER LEAGUE ", No_round, " ROUND ",
                       Hometeam, " vs ", Awayteam,
@@ -19,28 +19,28 @@ CairoPDF(file = paste("PL/PREMIER LEAGUE ", No_round, " ROUND ",
 Football_result(
     matchtype = paste("2018/19 PREMIER LEAGUE", No_round, "ROUND", sep=" "),
     Hometeam = Hometeam, Awayteam = Awayteam,
-    Home = "4-3-3",
-    HomeColour = "red", Home2Colour = "red",
-    HomeGKColour = c("#FF1493"),
+    Home = "4-2-3-1",
+    HomeColour = "blue", Home2Colour = "white",
+    HomeGKColour = c("#00FF00"),
     HomeLineup = c(
-        "Alisson",
-        "Alexander-Arnold", "Matip", "van Dijk", "Robertson",
-        "Milner", "Fabinho", "Wijnaldum",
-        "Salah", "Mané", "Origi"
+        "Pickford",
+        "Coleman", "Keane", "Zouma", "Digne",
+        "Gueye", "Schneiderlin",
+        "Walcott", "G Sigurdsson", "Bernard",
+        "Calvert_Lewin"
     ),
-    HomeSub = c("Henderson 70'", 62, "Keita 84'", 82, "Lallana 78'", 102),
-    Homescorer = c(10, 10, 11, 4, 4),
+    HomeSub = c("Richalison 59'", 82, "André Gomes 76'", 72, "Tosun 74", 112),
 
-    Away = "4-4-2",
-    AwayColour = "yellow", Away2Colour = "black",
-    AwayGKColour = c("#1C86EE"),
+    Away = "4-3-3",
+    AwayColour = "red", Away2Colour = "red",
+    AwayGKColour = c("#FFFF00"),
     AwayLineup = c(
-        "Foster",
-        "Masina", "Cathcart", "Mariappa", "Janmaat",
-        "Pereyra", "Capoue", "Doucouré", "Hughes",
-        "Deeney", "Deulofeu"
+        "Alisson",
+        "Robertson", "van Dijk", "Matip", "Alexander-Arnold",
+        "Henderson", "Fabinho", "Wijnaldum",
+        "Origi", "Mané", "Salah"
     ),
-    AwaySub = c("Sema 84'", 62, "Gray 73'", 102, "Cleverley 73'", 112)
+    AwaySub = c("Milner 63'", 82, "Lallana 84'", 102, "Roberto Firmino 63", 92)
 )
 
 
